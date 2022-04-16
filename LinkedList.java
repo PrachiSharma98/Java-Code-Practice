4d56b19e1e428063538fafdd05d69f66c2ce1133
Prachi Sharma <prachi.sharma@ihsmarkit.com>
2022-04-16 10:24:23 +0530

    Basic linkedlist program

---
+++ LinkedList.java
@@ -0,0 +1,39 @@
+package codingpractice;
+
+public class LinkedList {
+
+	static class Node{
+		int data;
+		Node next;
+		Node(int x)
+		{
+			data=x;
+			next=null;
+		}
+		
+	}
+	public static class Test{
+	public static void main(String [] args)
+	{
+		//creating a linkedlist
+		Node head = new Node(10);
+		Node temp1 = new Node(20);
+		Node temp2 = new Node(30);
+		head.next=temp1;
+		temp1.next=temp2;	
+		//traversing linkedlist
+		print(head);
+}
+	public static void print(Node head)
+	{
+		Node curr=head;
+		while(curr!=null)
+		{
+			System.out.println(curr.data);
+			curr=curr.next;
+		}
+	}
+	}
+	
+	
+}
